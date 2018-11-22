5d29b1fd72b52b384db727c3ed08e703da0c0941
Emanoel Queiroz <emanoelqueiroz@gmail.com>
2018-11-22 17:20:39 -0200

    Testing the run command

--- cli.js
+++ cli.js
@@ -40,13 +40,34 @@ function generateFiles(type, name, options) {
         logger.info(`avaible types: ${config.avaibleTypes.join(', ')}`);
         return;
     }
-    
+
     name = name.replace(/\.\w*/g, '');
     const generator = new Generator(config, {
         type, options
     });
 
     generator[type](name);
+}
+
+const { exec, spawn } = require('child_process');
+function run() {
+    const verification = new Verification();
+    if (!verification.isProjectValid()) return;
+
+    // exec('.\/iisexpress /?', { cwd: 'C:\/Program Files\/IIS Express\/' });
+    const yarnCmd = spawn('.\/iisexpress', [`/path:${process.cwd()}`, '/port:5423', '/clr:v4.0'], { cwd: 'C:\/Program Files (x86)\/IIS Express\/' });
+    // const yarnCmd = spawn('.\/iisexpress', [`/config:${process.cwd()}\/Web.config`], { cwd: 'C:\/Program Files (x86)\/IIS Express\/' });
+    // Nome de arquivo: \\?\c:\Users\Jessica-PC\teste.com.br\Web.configErro: N�o � poss�vel ler a se��o de configura��o 'system.applicationHost/sites' porque falta uma declara��o de se��o
+
+    yarnCmd.stdout.on('data', data => logger.info(data.toString().replace(/[\n\r]/g, '') + '\n'));
+}
+
+function build() {
+    const verification = new Verification();
+    if (!verification.isProjectValid()) return;
+
+    console.log(':)');
+    // C:\Windows\Microsoft.NET\Framework\v4.0.30319\aspnet_compiler.exe -v /univiasoft.com.br -p C:\Users\Keller\AppData\Local\Temp\WebSitePublish\univiasoft.com.br-303842288\obj\Debug\AspnetCompileMerge\Source -u -fixednames C:\Users\Keller\AppData\Local\Temp\WebSitePublish\univiasoft.com.br-303842288\obj\Debug\AspnetCompileMerge\TempBuildDir 
 }
 
 // Version
@@ -65,6 +86,18 @@ commander.command('generate <type> <value>')
     .option('--template', 'do template aside page')
     .description(`generate new files, avaible types: ${config.avaibleTypes.join(', ')}`)
     .action(generateFiles);
+
+// Run Project
+commander.command('run')
+    .alias('r')
+    .description(`run the actual project`)
+    .action(run);
+
+// Build Project
+commander.command('build')
+    .alias('b')
+    .description(`build the actual project`)
+    .action(build);
 
 // Commands that not exist
 commander.command('*')
