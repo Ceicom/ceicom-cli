2716ad63e62229f9e9beec59b5113506387c276a
Emanoel Queiroz <emanoelqueiroz@gmail.com>
2018-11-23 09:48:49 -0200

    started to make the build command

--- cli.js
+++ cli.js
@@ -49,6 +49,7 @@ function generateFiles(type, name, options) {
     generator[type](name);
 }
 
+const fs = require('fs-extra');
 const { exec, spawn } = require('child_process');
 function run() {
     const verification = new Verification();
@@ -59,15 +60,40 @@ function run() {
     // const yarnCmd = spawn('.\/iisexpress', [`/config:${process.cwd()}\/Web.config`], { cwd: 'C:\/Program Files (x86)\/IIS Express\/' });
     // Nome de arquivo: \\?\c:\Users\Jessica-PC\teste.com.br\Web.configErro: N�o � poss�vel ler a se��o de configura��o 'system.applicationHost/sites' porque falta uma declara��o de se��o
 
-    yarnCmd.stdout.on('data', data => logger.info(data.toString().replace(/[\n\r]/g, '') + '\n'));
+    yarnCmd.stdout.on('data', data => logger.info(data.toString().replace(/[\n\r]/g, '')));
+}
+
+function getDirs(path) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(path, (err, items) => {
+            if (err) reject(err);
+            let arr = [];
+            for (let i = 0; i < items.length; i++) {
+                if (!fs.lstatSync(`${path}/${items[i]}`).isDirectory()) continue;
+                arr.push(items[i]);
+            }
+            resolve(arr);
+        });
+    });
 }
 
 function build() {
     const verification = new Verification();
     if (!verification.isProjectValid()) return;
 
-    console.log(':)');
-    // C:\Windows\Microsoft.NET\Framework\v4.0.30319\aspnet_compiler.exe -v /univiasoft.com.br -p C:\Users\Keller\AppData\Local\Temp\WebSitePublish\univiasoft.com.br-303842288\obj\Debug\AspnetCompileMerge\Source -u -fixednames C:\Users\Keller\AppData\Local\Temp\WebSitePublish\univiasoft.com.br-303842288\obj\Debug\AspnetCompileMerge\TempBuildDir 
+    const compilerPath = 'C:/Windows/Microsoft.NET/Framework';
+    getDirs(compilerPath).then(data=> {
+        const onlyVersion = data.filter(item => item.indexOf('v') > -1);
+        logger.info(onlyVersion);
+    }).catch(logger.error);
+
+    // const yarnCmd = spawn('C:\/Windows\/Microsoft.NET\/Framework\/v4.0.30319\/aspnet_compiler.exe', ['-v', `${process.cwd()}`, '-p', `${process.cwd()}`, '-u', '-f', 'C:\/teste_compilados'], { cwd: './' });
+    // yarnCmd.stdout.on('data', data => logger.info(data.toString().replace(/[\n\r]/g, '')));
+    // yarnCmd.stderr.on('data', data => logger.info(`stderr: ${data}`));
+    // yarnCmd.on('close', () => {
+    //     logger.success('finished building');
+    // });
+    // C:\Windows\Microsoft.NET\Framework\v4.0.30319\aspnet_compiler.exe -v /teste.com.br -p C:\Users\Keller\AppData\Local\Temp\WebSitePublish\teste.com.br-303842288\obj\Debug\AspnetCompileMerge\Source -u -fixednames C:\Users\Keller\AppData\Local\Temp\WebSitePublish\teste.com.br-303842288\obj\Debug\AspnetCompileMerge\TempBuildDir 
 }
 
 // Version
